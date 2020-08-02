5fa82b3cda185ff795e527763696c9199ee8908f
Rohan Bansal <bansalrohan18@gmail.com>
2020-08-01 20:18:51 -0700

    try to fix launch problem?

--- desktop/src/com/amhsrobotics/circuitsim/desktop/DesktopLauncher.java
+++ desktop/src/com/amhsrobotics/circuitsim/desktop/DesktopLauncher.java
@@ -3,20 +3,22 @@ package com.amhsrobotics.circuitsim.desktop;
 import com.amhsrobotics.circuitsim.Main;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.apache.commons.lang3.SystemUtils;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		config.title = "TKO 1351 Circuit Simulator";
-		config.resizable = false;
-		config.width = 1366;
-		config.height = 768;
-//		config.setTitle("TKO 1351 Circuit Simulator");
-//		config.setResizable(false);
-//		config.setHdpiMode(HdpiMode.Logical);
-//		config.setWindowedMode(1366, 768);
+		if(SystemUtils.IS_OS_WINDOWS || SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_MAC) {
+			LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		new LwjglApplication(new Main(), config);
+			config.title = "TKO 1351 Circuit Simulator";
+			config.resizable = false;
+			config.width = 1366;
+			config.height = 768;
+
+			new LwjglApplication(new Main(), config);
+		} else {
+			System.out.println("Incompatible Operating System. If you aren't using Windows, Linux, or Mac, wtf are you using?");
+		}
 	}
 }
