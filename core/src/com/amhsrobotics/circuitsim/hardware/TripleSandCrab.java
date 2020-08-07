88150cf87829084fde82fb46fe1a089b6f076935
Rohan Bansal <bansalrohan18@gmail.com>
2020-08-07 12:08:38 -0700

    triple sandcrab implement start

--- core/src/com/amhsrobotics/circuitsim/hardware/TripleSandCrab.java
+++ core/src/com/amhsrobotics/circuitsim/hardware/TripleSandCrab.java
@@ -1,8 +1,27 @@
 package com.amhsrobotics.circuitsim.hardware;
 
-public class TripleSandCrab {
+import com.amhsrobotics.circuitsim.wiring.Cable;
+import com.badlogic.gdx.math.Vector2;
+import me.rohanbansal.ricochet.tools.ModifiedShapeRenderer;
 
-    public TripleSandCrab() {
+public class TripleSandCrab extends DoubleSandCrab {
 
+    public TripleSandCrab(Vector2 position) {
+        super(position);
+    }
+
+    @Override
+    public void attachWire(Cable cable, int port, boolean endOfWire) {
+        super.attachWire(cable, port, endOfWire);
+    }
+
+    @Override
+    public void drawHover(ModifiedShapeRenderer renderer) {
+        super.drawHover(renderer);
+    }
+
+    @Override
+    public void clearConnection(Cable cable) {
+        super.clearConnection(cable);
     }
 }
