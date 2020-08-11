996d4d2d5307e2483fd0978b9baeb5e0e7f0bba6
Rohan Bansal <bansalrohan18@gmail.com>
2020-08-11 11:43:45 -0700

    completely modularize hardware

--- core/src/com/amhsrobotics/circuitsim/hardware/SandCrab.java
+++ core/src/com/amhsrobotics/circuitsim/hardware/SandCrab.java
@@ -4,48 +4,33 @@ import com.amhsrobotics.circuitsim.files.JSONReader;
 import com.amhsrobotics.circuitsim.gui.CircuitGUIManager;
 import com.amhsrobotics.circuitsim.utility.ClippedCameraController;
 import com.amhsrobotics.circuitsim.utility.SnapGrid;
-import com.amhsrobotics.circuitsim.utility.Tools;
 import com.amhsrobotics.circuitsim.wiring.Cable;
 import com.amhsrobotics.circuitsim.wiring.CableManager;
 import com.amhsrobotics.circuitsim.wiring.CrimpedCable;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import me.rohanbansal.ricochet.tools.ModifiedShapeRenderer;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import java.util.ArrayList;
-
 public class SandCrab extends Hardware {
 
-    private Sprite bottom;
-    ArrayList<JSONArray> pinDefs = new ArrayList<>();
-    ArrayList<JSONArray> pinSizeDefs = new ArrayList<>();
-    ArrayList<Sprite> connectors = new ArrayList<>();
-
-    boolean canMove, addCrimped;
 
     public SandCrab(Vector2 position, HardwareType type, boolean... addCrimped) {
-        super(position);
+        super(position, addCrimped);
 
         this.type = type;
-        if(addCrimped.length > 0) {
-            this.addCrimped = addCrimped[0];
-        }
 
         if(type == HardwareType.DOUBLESANDCRAB) {
             JSONReader.loadConfig("scripts/DoubleSandCrab.json");
-            bottom = new Sprite(new Texture(Gdx.files.internal("img/hardware/sandcrab_white.png")));
+            base = new Sprite(new Texture(Gdx.files.internal("img/hardware/sandcrab_white.png")));
         } else {
             JSONReader.loadConfig("scripts/TripleSandCrab.json");
-            bottom = new Sprite(new Texture(Gdx.files.internal("img/hardware/sandcrab_white_2.png")));
+            base = new Sprite(new Texture(Gdx.files.internal("img/hardware/sandcrab_white_2.png")));
         }
 
         connNum = ((Long) JSONReader.getCurrentConfig().get("totalPins")).intValue();
@@ -58,8 +43,7 @@ public class SandCrab extends Hardware {
             pinSizeDefs.add((JSONArray) ((JSONObject) pins.get(x)).get("dimensions"));
         }
 
-        bottom.setCenter(position.x, position.y);
-
+        base.setCenter(position.x, position.y);
 
         for(JSONArray arr : pinDefs) {
             Sprite temp;
@@ -77,127 +61,6 @@ public class SandCrab extends Hardware {
 
         initConnections();
         initEnds();
-
-        if(this.addCrimped) {
-            checkCrimpedCables();
-        }
-
-        canMove = false;
-    }
-
-    public void update(SpriteBatch batch, ModifiedShapeRenderer renderer, ClippedCameraController camera) {
-        super.update(batch, renderer, camera);
-
-        bottom.setCenter(getPosition().x, getPosition().y);
-        for(Sprite temp : connectors) {
-            temp.setCenter(getPosition().x + (Long) pinDefs.get(connectors.indexOf(temp)).get(0), getPosition().y + (Long) pinDefs.get(connectors.indexOf(temp)).get(1));
-        }
-
-        Vector2 vec = Tools.mouseScreenToWorld(camera);
-
-
-        //HOVERING MECHANICS
-        //---------------------------------------------------
-
-        if(bottom.getBoundingRectangle().contains(vec.x, vec.y)) {
-
-            //HOVERING
-            drawHover(renderer);
-
-            //SELECTING
-            if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-                HardwareManager.currentHardware = this;
-                CableManager.currentCable = null;
-
-                populateProperties();
-                CircuitGUIManager.propertiesBox.show();
-            }
-
-        }
-
-
-        //SELECTED MECHANICS
-        //---------------------------------------------------
-
-        if(HardwareManager.currentHardware == this) {
-            drawHover(renderer);
-
-            if((Gdx.input.isKeyJustPressed(Input.Keys.FORWARD_DEL) || Gdx.input.isKeyJustPressed(Input.Keys.DEL))) {
-                this.delete();
-            }
-
-            //UNSELECT
-            if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-                CircuitGUIManager.propertiesBox.hide();
-                HardwareManager.currentHardware = null;
-            }
-
-
-            //MOVING
-            if (Gdx.input.isTouched()) {
-
-                //BEING DRAGGED: ALLOW MOVE
-
-                if(Gdx.input.getDeltaX() != 0 && Gdx.input.getDeltaY() != 0 && bottom.getBoundingRectangle().contains(vec.x, vec.y)) {
-                    canMove = true;
-                }
-                HardwareManager.movingObject = true;
-
-            } else {
-
-                //STOP MOVING
-
-                if(HardwareManager.movingObject) {
-                    canMove = false;
-                    HardwareManager.movingObject = false;
-                }
-            }
-
-            if(Gdx.input.isTouched() && canMove) {
-                if(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
-                    SnapGrid.calculateSnap(vec);
-                }
-
-                //SET OWN POSITION
-                setPosition(vec.x, vec.y);
-
-
-                //MOVE CABLES
-
-                for(JSONArray arr : pinDefs) {
-                    int index = pinDefs.indexOf(arr);
-                    if(connections.get(index) != null) {
-                        connections.get(index).editCoordinates(
-                                new Vector2(getConnector(index).getX() + getConnector(index).getWidth() / 2, getConnector(index).getY() + 20),
-                                ends.get(index), false);
-                        connections.get(index).editCoordinates(
-                                new Vector2(getConnector(index).getX() + getConnector(index).getWidth() / 2, getConnector(index).getY() - 20),
-                                ends.get(index), true);
-                    }
-                }
-            }
-        }
-
-        if(this.addCrimped) {
-            checkCrimpedCables();
-        }
-
-        batch.begin();
-        bottom.draw(batch);
-        for(Sprite conn : connectors) {
-            conn.draw(batch);
-        }
-        batch.end();
-    }
-
-    public void checkCrimpedCables() {
-        for(int i : crimpedPorts) {
-            if(connections.get(i) == null) {
-                CrimpedCable c = new CrimpedCable();
-                CableManager.addCable(c);
-                attachCrimpedCable(c, i);
-            }
-        }
     }
 
     @Override
@@ -271,6 +134,7 @@ public class SandCrab extends Hardware {
 
     }
 
+    @Override
     public void attachCrimpedCable(Cable cable, int port) {
         connections.set(port, cable);
 
@@ -283,19 +147,7 @@ public class SandCrab extends Hardware {
         CableManager.currentCable = null;
     }
 
-    public Sprite getConnector(int conn) {
-        return connectors.get(conn);
-    }
-
     public int getTotalConnectors() {
         return connNum;
-    }
-
-    public void drawHover(ModifiedShapeRenderer renderer) {
-        renderer.setColor(Color.WHITE);
-
-        renderer.begin(ShapeRenderer.ShapeType.Filled);
-        renderer.roundedRect(getPosition().x - (bottom.getWidth() / 2), getPosition().y - (bottom.getHeight() / 2), bottom.getWidth()-1, bottom.getHeight(), 15);
-        renderer.end();
     }
 }
