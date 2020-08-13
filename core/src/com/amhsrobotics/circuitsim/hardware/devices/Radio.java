ff835cefac98a73896034f9194a50a1f766684af
Rohan Bansal <bansalrohan18@gmail.com>
2020-08-12 21:40:13 -0700

    add radio class

---
+++ core/src/com/amhsrobotics/circuitsim/hardware/devices/Radio.java
@@ -0,0 +1,63 @@
+package com.amhsrobotics.circuitsim.hardware.devices;
+
+import com.amhsrobotics.circuitsim.files.JSONReader;
+import com.amhsrobotics.circuitsim.hardware.Hardware;
+import com.amhsrobotics.circuitsim.hardware.HardwareType;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import me.rohanbansal.ricochet.tools.ModifiedShapeRenderer;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class Radio extends Hardware {
+
+    public Radio(Vector2 position, HardwareType type) {
+        super(position);
+
+        this.type = type;
+
+        JSONReader.loadConfig("scripts/Radio.json");
+        base = new Sprite(new Texture(Gdx.files.internal("img/hardware/Radio.png")));
+
+        connNum = ((Long) JSONReader.getCurrentConfig().get("totalPins")).intValue();
+        name = (String) (JSONReader.getCurrentConfig().get("name"));
+        JSONArray pins = (JSONArray) JSONReader.getCurrentConfig().get("pins");
+        for(int x = 0; x < pins.size(); x++) {
+            pinDefs.add((JSONArray) ((JSONObject) pins.get(x)).get("position"));
+            pinSizeDefs.add((JSONArray) ((JSONObject) pins.get(x)).get("dimensions"));
+            portTypes.add((String) ((JSONObject) pins.get(x)).get("type"));
+        }
+
+        base.setCenter(position.x, position.y);
+
+        for(JSONArray arr : pinDefs) {
+            Sprite temp;
+            if(connectors.size() == connNum) {
+                break;
+            }
+            temp = new Sprite(new Texture(Gdx.files.internal("img/point.png")));
+            temp.setCenter(position.x + (Long) arr.get(0), position.y + (Long) arr.get(1));
+            temp.setSize((Long)pinSizeDefs.get(pinDefs.indexOf(arr)).get(0), (Long)pinSizeDefs.get(pinDefs.indexOf(arr)).get(1));
+            connectors.add(temp);
+        }
+
+        initConnections();
+        initEnds();
+    }
+
+    public Vector2 calculate(int port) {
+        return new Vector2(getConnector(port).getX() + getConnector(port).getWidth() / 2, getConnector(port).getY() + getConnector(port).getHeight() / 2 + 40);
+    }
+
+    public void drawHover(ModifiedShapeRenderer renderer) {
+        renderer.setColor(new Color(156/255f,1f,150/255f,1f));
+
+        renderer.begin(ShapeRenderer.ShapeType.Filled);
+        renderer.roundedRect(getPosition().x - (base.getWidth() / 2)-7, getPosition().y - (base.getHeight() / 2)-7, base.getWidth()+16, base.getHeight()+13, 5);
+        renderer.end();
+    }
+}
--- core/src/com/amhsrobotics/circuitsim/utility/input/DigitFilter.java
+++ core/src/com/amhsrobotics/circuitsim/utility/input/DigitFilter.java
@@ -11,8 +11,6 @@ public class DigitFilter implements TextField.TextFieldFilter {
         accepted = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'};
     }
 
-
-
     @Override
     public boolean acceptChar(TextField textField, char c) {
         if(c == '.') {
