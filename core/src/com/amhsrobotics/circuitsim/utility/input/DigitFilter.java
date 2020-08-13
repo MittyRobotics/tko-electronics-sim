c2cd0665e2646fb99019952d71bc49c00f7229ca
Rohan Bansal <bansalrohan18@gmail.com>
2020-08-12 19:25:47 -0700

    modify digitfilter for future use

--- core/src/com/amhsrobotics/circuitsim/utility/input/DigitFilter.java
+++ core/src/com/amhsrobotics/circuitsim/utility/input/DigitFilter.java
@@ -5,13 +5,19 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 public class DigitFilter implements TextField.TextFieldFilter {
 
     private char[] accepted;
+    private boolean dotPressed = false;
 
     public DigitFilter() {
         accepted = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'};
     }
 
+
+
     @Override
     public boolean acceptChar(TextField textField, char c) {
+        if(c == '.') {
+            dotPressed = true;
+        }
         for (char a : accepted)
             if (a == c) return true;
         return false;
