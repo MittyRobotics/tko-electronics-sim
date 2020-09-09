03b5f58b4d86d3f0417d6ac2761c57508c98337f
Rohan Bansal <bansalrohan18@gmail.com>
2020-09-09 09:13:54 -0700

    optimiz/remove switchcases

---
+++ core/src/com/amhsrobotics/circuitsim/hardware/devices/Solenoid.java
@@ -0,0 +1,41 @@
+package com.amhsrobotics.circuitsim.hardware.devices;
+
+import com.amhsrobotics.circuitsim.hardware.Flippable;
+import com.amhsrobotics.circuitsim.hardware.HardwareType;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import org.json.simple.JSONArray;
+
+public class Solenoid extends Flippable {
+
+    public Solenoid(Vector2 position, boolean... addCrimped) {
+        super(position, HardwareType.SOLENOID, addCrimped);
+
+
+        for(JSONArray arr : pinDefs) {
+            Sprite temp;
+            if(connectors.size() == connNum) {
+                break;
+            }
+            temp = new Sprite(new Texture(Gdx.files.internal("img/point.png")));
+            temp.setCenter(position.x + (Long) arr.get(0), position.y + (Long) arr.get(1));
+            temp.setSize((Long)pinSizeDefs.get(pinDefs.indexOf(arr)).get(0) /2f, (Long)pinSizeDefs.get(pinDefs.indexOf(arr)).get(1) /2f);
+            connectors.add(temp);
+        }
+
+        initConnections();
+        initEnds();
+    }
+
+    public Vector2 calculate(int port) {
+        if(port == 0) {
+            return calculateDirection(3+cur, port);
+        } else if (port == 1) {
+            return calculateDirection(1+cur, port);
+        } else {
+            return calculateDirection(cur, port);
+        }
+    }
+}
