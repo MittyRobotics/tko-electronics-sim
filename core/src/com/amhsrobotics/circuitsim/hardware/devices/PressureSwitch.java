184769a9e2b51e993f82b820bd68eff8abcd957f
Andy Li <qingyuanli23@mittymonarch.com>
2020-09-09 20:00:41 -0700

    add four classes

---
+++ core/src/com/amhsrobotics/circuitsim/hardware/devices/PressureSwitch.java
@@ -0,0 +1,41 @@
+package com.amhsrobotics.circuitsim.hardware.devices;
+
+import com.amhsrobotics.circuitsim.hardware.Flippable;
+import com.amhsrobotics.circuitsim.hardware.HardwareType;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import org.json.simple.JSONArray;
+
+public class PressureSwitch extends Flippable {
+
+    public PressureSwitch(Vector2 position, boolean... addCrimped) {
+        super(position, HardwareType.PRESSURESWITCH, addCrimped);
+
+
+        for(JSONArray arr : pinDefs) {
+            Sprite temp;
+            if(connectors.size() == connNum) {
+                break;
+            }
+            temp = new Sprite(new Texture(Gdx.files.internal("img/point.png")));
+            temp.setCenter(position.x + (Long) arr.get(0), position.y + (Long) arr.get(1));
+            temp.setSize((Long)pinSizeDefs.get(pinDefs.indexOf(arr)).get(0), (Long)pinSizeDefs.get(pinDefs.indexOf(arr)).get(1));
+            connectors.add(temp);
+        }
+
+        initConnections();
+        initEnds();
+    }
+
+    public Vector2 calculate(int port) {
+        if(port < 3) {
+            return calculateDirection(cur+2, port);
+        } else if (port < 5) {
+            return calculateDirection(cur, port);
+        } else {
+            return calculateDirection(cur+3, port);
+        }
+    }
+}
