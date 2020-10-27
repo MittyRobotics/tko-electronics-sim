4e2b766ae999172e263c2e794d14da2f1a52116a
Andy Li <qingyuanli23@mittymonarch.com>
2020-10-27 11:32:44 -0700

    vrm leds

--- core/src/com/amhsrobotics/circuitsim/hardware/devices/VoltageRegulatorModule.java
+++ core/src/com/amhsrobotics/circuitsim/hardware/devices/VoltageRegulatorModule.java
@@ -43,11 +43,29 @@ public class VoltageRegulatorModule extends Flippable {
 
     public String check() {
         if(getNull(0) || getNull(1) || !(getOther(0) instanceof PowerDistributionPanel && getOther(1) instanceof PowerDistributionPanel)) {
+            if(simLED) {
+                resetLEDs();
+                simLED = false;
+            }
             return "VRM is not connected to PDP";
         }
 
         if(!((getNum(0) == 0 && getNum(1) == 1) || (getNum(0) == 2 && getNum(1) == 3))) {
+            if(simLED) {
+                resetLEDs();
+                simLED = false;
+            }
             return "VRM is improperly connected to PDP";
+        }
+
+        if(!simLED) {
+            simLED = true;
+
+            LEDs.get(0).setColor("green");
+            LEDs.get(0).setStatus("Power On");
+            LEDs.get(1).setColor("green");
+            LEDs.get(1).setStatus("Power On");
+
         }
 
         return null;
