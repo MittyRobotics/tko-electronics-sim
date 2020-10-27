ddf312a50520612b6b7399f1bd386b822d4ad999
Andy Li <qingyuanli23@mittymonarch.com>
2020-10-26 18:17:41 -0700

    fix compressor

--- core/src/com/amhsrobotics/circuitsim/hardware/devices/Compressor.java
+++ core/src/com/amhsrobotics/circuitsim/hardware/devices/Compressor.java
@@ -32,10 +32,21 @@ public class Compressor extends Flippable {
     }
 
     public Vector2 calculate(int port) {
-        if(port >= 1) {
-            return calculateDirection(cur+3, port, 100);
-        } else {
-            return calculateDirection(cur+1, port, 100);
+        return calculateDirection(cur+1, port, 100);
+    }
+    
+    public String check() {
+        if((!(getNotNull(1) && getOther(1) instanceof PneumaticsControlModule) || !(getNotNull(2) && getOther(2) instanceof PneumaticsControlModule))) {
+            return "Compressor not connected to PCM";
         }
+
+        if(getNum(1) != 9 || getNum(2) != 8) {
+            return "Compressor incorrectly connected to PCM";
+        }
+
+        if(!(getNotNull(0) && getOther(0) instanceof Tank)) {
+            return "Compressor not connected to tanks";
+        }
+        return null;
     }
 }
